3465839e97e52d070c37849cbfc431936fbf7db3
adamisrusty <adamgutierreztx@gmail.com>
2020-09-18 19:27:25 -0500

    Initial commit

---
+++ cosc_1436_class_activity_4_csharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cosc_1436_class_activity_4_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
